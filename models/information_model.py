03ca8f8a33ef4c2b0386424166ceb3d103f79796
Anton Ivanichenko <a.a.ivanichenko@gmail.com>
2024-10-05 13:50:24 +0200

    Add methods for word addition

--- models/information_model.py
+++ models/information_model.py
@@ -3,7 +3,7 @@ from typing import Set, List
 
 class InformationModel:
     def __init__(self):
-        self._small_words : Set[str] = {
+        self._small_words: Set[str] = {
             "and", "or", "but", "a", "an", "the", "as", "at",
             "by", "for", "in", "of", "on", "to", "with", "is"
         }
@@ -13,9 +13,15 @@ class InformationModel:
         return self._small_words
 
     @small_words.setter
-    def small_words(self, value : Set[str]) -> None:
+    def small_words(self, value: Set[str]) -> None:
         self._small_words = value
 
     @property
     def sorted_small_words(self) -> List[str]:
         return sorted(list(self.small_words))
+
+    def add_single_small_word(self, word: str) -> None:
+        return self.small_words.add(word)
+
+    def add_multiple_small_words(self, words: List[str]) -> None:
+        return self.small_words.update(words)
